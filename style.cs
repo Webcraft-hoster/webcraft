ddcfd366c4e626e90561da020d27b9c620d7faf4
Webcraft <32jwells@stu.asher.k12.ok.us>
2025-05-02 10:25:04 -0500

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,32 @@
+body {
+  font-family: 'Helvetica', sans-serif;
+  background-color: #f4f4f4;
+  color: #333;
+  text-align: center;
+  margin: 0;
+}
+
+h1 {
+  font-size: 48px;
+  color: #2c2c2c;
+}
+
+h3 {
+  font-size: 24px;
+  color: #555;
+}
+
+a {
+  color: #0070d0;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+footer {
+  margin-top: 30px;
+  font-size: 12px;
+  color: #888;
+}
